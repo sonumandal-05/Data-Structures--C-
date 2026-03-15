22fa484dbc5c1e12e6d4259b61c4b0068797c6f7
Sonu Mandal <imsonumandal05@gmail.com>
2026-03-15 20:45:25 +0530

    Add initial implementation of stack data structure in C

---
+++ stack.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+#define MAX 5
+
+int stack[MAX];
+int top = -1;
+
+int main()
+{
+    printf("Stack created successfully.\n");
+    printf("Initial Top Position = %d",top);
+
+    return 0;
+}
